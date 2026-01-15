3aee57931175940b4f6c26022e7012adc033efba
prakritish <bhattacharyaprakritish@gmail.com>
2026-01-15 19:58:45 +0530

    learning Postman

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 const express = require("express") // import "express" from node modules
 const app = express() // create an app instance of express()
 
+app.use("/test",(req,res) =>{
+    res.send("You are in test page!!!")
+})
 app.use("/",(req,res) =>{
     res.send("Hello from the server !!!")
 })
-app.use("/test",(req,res) =>{
-    res.send("You are in /test page!!!")
-})
+
 app.listen(3000, () =>{
     console.log("server is successfully listening on port 3000...")
 })// The server listens for incoming HTTP requests on port number 3000.
