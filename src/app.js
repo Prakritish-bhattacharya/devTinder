2e564166a385ae402eaafd0d648076994f86e856
prakritish <bhattacharyaprakritish@gmail.com>
2026-06-17 20:18:06 +0530

    Initial commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
 const express = require("express")
 const connectDB = require("./config/database")
+const User = require("./models/user")
 const app = express()
+
+
+app.post("/signup", async(req,res)=>{
+    const user = new User({
+        firstName : "Piku",
+        lastName : "Singh",
+        emailId : "piku@gmail.com",
+        password : "123456"
+    })
+    await user.save()
+    res.send("User created successfully")
+})
 
 connectDB().then(()=>{
     console.log("Database Connected Sucessfully")
