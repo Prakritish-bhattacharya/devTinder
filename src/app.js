e989108ad4cfbe09ff523c59f70d62b44be441b4
prakritish <bhattacharyaprakritish@gmail.com>
2026-06-19 00:55:10 +0530

    GetUserByEmailId_FeedAPI_DeleteUser_UpdateUserData

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express()
 app.use( express.json())
 
 
-// create Route
+// create Signup Route
 app.post("/signup", async (req,res)=>{
     const user  = new User(req.body)
     try {
@@ -19,6 +19,66 @@ app.post("/signup", async (req,res)=>{
         res.status(400).send("In Signup Route...", error)        
     }
     
+})
+
+// Get Users by EmailId
+app.get("/user", async(req,res)=>{
+    const userEmail = req.body.emailId
+
+    try {
+        const users = await User.find({emailId : userEmail})
+        if(users.length === 0){
+            res.status(404).send("User not found")
+        }else{
+            res.send(users)
+        }
+    } catch (error) {
+        res.status(400).send("Something went wrong !!!")
+    }
+})
+
+// Feed API
+app.get("/feed", async(req,res)=>{
+    try {
+        const users = await User.find({})
+        res.send(users)
+    } catch (error) {
+        res.status(400).send("Something went wrong !!!")
+    }
+})
+
+//  Estimate Document count
+app.get("/estimateDoc", async(req,res)=>{
+    try {
+        const numUsers = await User.estimatedDocumentCount()
+        res.send(numUsers)
+    } catch (error) {
+        res.status(400).send("Document not found !!!")
+    }
+    
+})
+
+//  Delete a user from DB
+app.delete("/user", async(req,res)=>{
+    const userId = req.body.userId
+    try {
+        const user = await User.findByIdAndDelete({_id: userId})
+        res.send("User deleted Successfully...")
+    } catch (error) {
+        res.status(400).send("Something went wrong !!!")
+    }
+})
+
+// Update user data
+app.patch("/user", async(req,res)=>{
+    const userId = req.body.userId
+    const data = req.body
+    try {
+        await User.findByIdAndUpdate({_id: userId}, data)
+        res.send("User Updated successfully...")
+    } catch (error) {
+        res.status(400).send("Something went wrong !!!")
+    }
 })
 
 
